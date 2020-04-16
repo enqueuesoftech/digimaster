0625261f7ca75e8b59a065ff208afe4491b05df6
Mohammed <mohammedsaket1@gmail.com>
2020-04-16 11:46:50 +0530

    Updates

---
+++ precache-manifest.5e8af140dc26e33bec223c9a55aeb649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbfd143f2c1b2fc44f913521a9eca54",
+    "url": "/digimaster/index.html"
+  },
+  {
+    "revision": "86c3e02937f0823ff7b6",
+    "url": "/digimaster/static/css/main.11b3f069.chunk.css"
+  },
+  {
+    "revision": "5af7fc417629ca101365",
+    "url": "/digimaster/static/js/2.3bc121a5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/digimaster/static/js/2.3bc121a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c3e02937f0823ff7b6",
+    "url": "/digimaster/static/js/main.ef726885.chunk.js"
+  },
+  {
+    "revision": "3ff0cccc3c6a241a0de5",
+    "url": "/digimaster/static/js/runtime-main.c901b5b3.js"
+  },
+  {
+    "revision": "63276065fa21d8e9f3dfd5e51bbb2fef",
+    "url": "/digimaster/static/media/background.63276065.jpg"
+  }
+]);
